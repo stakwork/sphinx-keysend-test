8ababb7eacbad31d5ad3a48fbdec805afeb77ec0
Evan Feenstra <evan@evanfeenstra.com>
2020-03-27 09:26:30 -0700

    proper json in log

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ var sha = require('js-sha256')
 const env = process.env.NODE_ENV || 'production'
 const config = require(__dirname + '/config.json')[env]
 
+function jlog(s) {
+  console.log(JSON.stringify(s, null, 2))
+}
+
 async function test(){
   const args = minimist(process.argv.slice(2))
   const dest = args['dest']
@@ -22,13 +26,24 @@ async function test(){
   const opts = {dest}
   try {
     const r = await keysend(opts)
-    console.log("=> KEYSEND SUCCESS\n",{
+    console.log("=> KEYSEND SUCCESS")
+    const hops = r.hops||[]
+    jlog({
       ...r,
       ...r.payment_hash && {payment_hash: r.payment_hash.toString('hex')},
       ...r.payment_preimage && {payment_preimage: r.payment_preimage.toString('hex')},
+      ...r.payment_route && {payment_route: {
+        ...r.payment_route,
+        hops: (r.payment_route.hops||[]).map(h=> {
+          return {...h, custom_records:{}}
+        })
+      }},
+      
     })
   } catch(e) {
-    console.log("=> KEYSEND FAILURE\n",{
+    console.log(e)
+    console.log("=> KEYSEND FAILURE")
+    jlog({
       payment_error: e
     })
   }
