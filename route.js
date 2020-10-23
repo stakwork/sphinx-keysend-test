c6c3ebdb7a642f00131f3a08bcdcd2137406f65b
Evan Feenstra <evan@evanfeenstra.com>
2020-10-23 11:21:43 -0700

    router

---
+++ route.js
@@ -0,0 +1,29 @@
+
+var minimist = require('minimist')
+var router = require('./router')
+
+function jlog(s) {
+    console.log(JSON.stringify(s, null, 2))
+}
+
+/*
+nodes route.js --hops=023d70f2f76d283c6c4e58109ee3a2816eb9d8feb40b23d62469060a2b2867b77f,0227230b7b685f1742b944bfc5d79ddc8c5a90b68499775ee10895f87307d8d22e,02a0591e848d24246a349fe61c5d7a86bab7c3d9598366675b9c91deb3e31ddf57,03e278ab0ed1f0b21af7280428d4f00a542aa8be703ccce1be285596645d64a827
+*/
+
+async function test() {
+  const args = minimist(process.argv.slice(2))
+  const hops = args['hops']
+  if (!hops) {
+      return console.log("NO HOPS")
+  }
+
+  const hs = hops.split(',')
+  try { 
+    const r = await router.buildRoutes(hs,3)
+    consolel.log(r)
+  } catch(e) {
+    console.log("ERROR",e)
+  }
+}
+
+test()
---
+++ router.js
@@ -0,0 +1,51 @@
+
+var grpc = require('grpc')
+var ByteBuffer = require('bytebuffer')
+var LND = require('./lightning')
+
+const env = process.env.NODE_ENV || 'production'
+const config = require(__dirname + '/config.json')[env]
+
+var routerClient;
+
+const loadRouter = () => {
+  if (routerClient) {
+    return routerClient
+  } else {
+    try {
+      var credentials = LND.loadCredentials('router.macaroon')
+      var lnrpcDescriptor = grpc.load("router.proto");
+      var signer = lnrpcDescriptor.routerrpc
+      routerClient = new signer.Signer(config.node_ip + ':' + config.lnd_port, credentials);
+      return routerClient
+    } catch (e) {
+      throw e
+    }
+  }
+}
+
+const buildRoutes = (dests, amt) => { // dests: array of hex strings
+  return new Promise(async (resolve, reject) => {
+    let router = await loadRouter()
+    try {
+      const options = {
+        hop_pubkeys: dests.map(d=>ByteBuffer.fromHex(d)),
+        final_cltv_delta: 144,
+        amt_msat: amt ? amt*1000 : 3000,
+      }
+      router.buildRoutes(options, function (err, route) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(route)
+        }
+      })
+    } catch(e) {
+      reject(e)
+    }
+  })
+}
+
+module.exports={
+  buildRoutes
+}
