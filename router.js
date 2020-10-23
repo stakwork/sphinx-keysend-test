a6111e50dcf12b0fa0189f0cb6fc285f0d29cda3
Evan Feenstra <evan@evanfeenstra.com>
2020-10-23 14:15:52 -0700

    ok

--- router.js
+++ router.js
@@ -2,15 +2,7 @@
 var ByteBuffer = require('bytebuffer')
 var LND = require('./lightning')
 var path = require('path')
-var protoLoader = require("@grpc/proto-loader")
 var grpc = require('grpc');
-// var Buffer = require('buffer').Buffer;
-
-const includePath = path.join(__dirname, "./proto")
-console.log("includePath:", includePath)
-const opts = {
-  includeDirs: [includePath]
-}
 
 const env = process.env.NODE_ENV || 'production'
 const config = require(__dirname + '/config.json')[env]
@@ -23,9 +15,8 @@ const loadRouter = () => {
   } else {
     try {
       var credentials = LND.loadCredentials('router.macaroon')
-      var descriptor = protoLoader.loadSync('router.proto', opts)
-      const defn= grpc.loadPackageDefinition(descriptor);
-      var router = defn.routerrpc
+      var descriptor = grpc.load("proto/router.proto");
+      var router = descriptor.routerrpc
       routerClient = new router.Router(config.node_ip + ':' + config.lnd_port, credentials);
       return routerClient
     } catch (e) {
@@ -43,7 +34,6 @@ const buildRoute = (dests, amt) => { // dests: array of hex strings
         final_cltv_delta: 144,
         amt_msat: amt ? amt*1000 : 3000,
       }
-      console.log("OPTIONS,",options)
       router.buildRoute(options, function (err, route) {
         if (err) {
           reject(err)
